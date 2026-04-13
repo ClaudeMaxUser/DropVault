54f17e78451d53bc3d784923c3d76a29ac819076
Arijitb98 <arijitb2611@gmail.com>
2026-04-13 16:47:14 +0530

    feat: update RTC_CONFIG with additional STUN and TURN server configurations for improved connectivity

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,10 +2,26 @@ export const RTC_CONFIG = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
     { urls: "stun:stun1.l.google.com:19302" },
+    { urls: "stun:stun.relay.metered.ca:80" },
     {
-      urls: "turn:turn.cloudflare.com:3478",
-      username: "free",
-      credential: "free",
+      urls: "turn:global.relay.metered.ca:80",
+      username: "e8c6b982a60608caa5f5c79b",
+      credential: "6TJW4q4B5S/yZjHW",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80?transport=tcp",
+      username: "e8c6b982a60608caa5f5c79b",
+      credential: "6TJW4q4B5S/yZjHW",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:443",
+      username: "e8c6b982a60608caa5f5c79b",
+      credential: "6TJW4q4B5S/yZjHW",
+    },
+    {
+      urls: "turns:global.relay.metered.ca:443?transport=tcp",
+      username: "e8c6b982a60608caa5f5c79b",
+      credential: "6TJW4q4B5S/yZjHW",
     },
   ],
 };
